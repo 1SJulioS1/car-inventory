fed47464bc67237a595a0ad8c0772a200ecda79c
s& <sjsiless@gmail.com>
2022-02-28 13:09:04 -0500

    static folder added

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name="Nombre", unique=True,primary_key=True)
+    cantidad = models.IntegerField()
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
